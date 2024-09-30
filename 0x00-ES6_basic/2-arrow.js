5ea6077fcfbc236e7812c9634bb6541850642b47
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-09-30 20:29:25 +0300

    update arrow

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,10 @@
-export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+/* eslint-disable */
+export default function getNeighborhoodsList () {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square']
   
-    // Arrow function automatically binds `this`, so no need for `self`
     this.addNeighborhood = (newNeighborhood) => {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return this.sanFranciscoNeighborhoods;
-    };
+      this.sanFranciscoNeighborhoods.push(newNeighborhood)
+      return this.sanFranciscoNeighborhoods
+    }
   }
   
