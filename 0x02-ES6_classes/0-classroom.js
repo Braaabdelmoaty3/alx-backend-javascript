6b7abe450184b276a20d9aa609af419258eae6bf
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-10-03 19:28:31 +0300

    classes task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+export default class ClassRoom{
+    constructor(maxStudentsSize){
+        this._maxStudentsSize = maxStudentsSize;
+    }
+}
+
+
+
+
+
+
+
+
+/*class User {
+    constructor(id, username, salary) {
+        this.id = id;
+        this.username = username;
+        this.salary = salary;
+        this.msg = function(){
+            return `${this.username} this is your salary ${this.salary}`
+        }
+    }
+}
+
+let ahemd = new User(100, "ahmed", 2000) ;
+console.log(ahemd.msg());*/
