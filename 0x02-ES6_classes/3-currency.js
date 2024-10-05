14e27fc70b6224a2b3c3e792d8f8ed46db0052e5
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-10-05 05:30:10 +0300

    task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,29 +1,27 @@
-export default class Currency {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
+/* eslint-disable */
+export default class Currency{
+    constructor(code, name){
+        this._code = code
+        this._name = name
     }
-  
-    // code
-    get code() {
-      return this._code;
+
+    get code(){
+        this._code;
     }
-  
-    set code(newCode) {
-      this._code = newCode;
+
+    set code(newCode){
+        this._code = newCode;
     }
-  
-    // name
-  
-    get name() {
-      return this._name;
+
+    get name(){
+        this._name
     }
-  
-    set name(newName) {
-      this._name = newName;
+    set name(newName){
+        this._name = newName
     }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
+
+
+    displayFullCurrency(name,code){
+        return `${this.name} (${this.code})`;
     }
-  }
+}
