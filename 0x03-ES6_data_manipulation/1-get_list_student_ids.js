00738ab882f63a3f21ef15657b5b131a04e1e11c
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-10-09 00:13:27 +0300

    correct the list func

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+/* edlint-disable */
+import getListStudents from "./0-get_list_students.js";
+
+export default function getListStudentIds(){
+    const ids = student.maps(student => student.id);
+}
+
+
