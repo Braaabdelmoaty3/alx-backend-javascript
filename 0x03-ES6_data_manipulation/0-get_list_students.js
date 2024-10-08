885f9e5090003f82b50cc2eab7d8783190825f2a
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-10-08 20:00:15 +0300

    get list of students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default function getListStudents(){
+/*
+* want this fun return a array of opjects
+* 1- declear a array of object 
+* return this array 
+*/ 
+
+
+    const array = [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ]
+    return array
+}
