2d47012f69e936ffa7d7e9e25e8cf7f397597a87
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-10-09 00:18:29 +0300

    task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,11 @@
 /* edlint-disable */
 import getListStudents from "./0-get_list_students.js";
 
-export default function getListStudentIds(){
-    const ids = student.maps(student => student.id);
+export default function getListStudentIds(students){
+    if (!Array.isArray(students)){
+        return [];
+    }
+
+    const ids = students.map(student => student.id);
+    return ids;
 }
-
-
