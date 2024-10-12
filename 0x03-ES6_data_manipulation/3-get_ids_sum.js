6b1ac6c35818e8a8723304f96c4702b7ca716977
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-10-12 21:01:14 +0300

    task 3

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(array){
+/*Create a function getStudentIdsSum that returns the sum of all the student ids.*/
+    const studentSum = array.reduce((accumulator, student) => {
+        return accumulator += student.id;
+    }, 0)
+    return studentSum
+}
